653438e7be4940f55119e9716e732e10f9907459
Rajeev Singh <rsinghh68@gmail.com>
2024-03-16 15:12:58 +0530

    segregated folder of generated files of proto for different repos

--- build.sh
+++ build.sh
@@ -1,14 +1,17 @@
 rm -Rf generated
 mkdir generated
+
+mkdir generated/notification
 cd notification-model
-protoc --go_out=../generated --go_opt=paths=source_relative \
-    --go-grpc_out=../generated --go-grpc_opt=paths=source_relative \
+protoc --go_out=../generated/notification --go_opt=paths=source_relative \
+    --go-grpc_out=../generated/notification --go-grpc_opt=paths=source_relative \
     *.proto
 cd ..
 
+mkdir generated/social
 cd social-model
-protoc --go_out=../generated --go_opt=paths=source_relative \
-    --go-grpc_out=../generated --go-grpc_opt=paths=source_relative \
+protoc --go_out=../generated/social --go_opt=paths=source_relative \
+    --go-grpc_out=../generated/social --go-grpc_opt=paths=source_relative \
     *.proto
 cd ..
 go build -mod=mod -o build/ .
